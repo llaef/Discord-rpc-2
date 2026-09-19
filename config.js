382f93a160394e5d9b0d91e2801802e75dfdb141
llaef <abdoufakir970@gmail.com>
2026-09-19 09:34:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,37 +8,6 @@ module.exports = {
         "name": "Minecraft",
         "details": "Playing Bedrock Edition",
         "state": "Survival Mode - Multiplayer",
-
-        // حط رابط ديسكورد المباشر للصورة هنا:
-        "largeImageKey": "https://cdn.discordapp.com/attachments/1547776611523371028/1549749482835677314/IMG_0806.jpg",
-        "largeImageText": "Minecraft Bedrock",
-
-        "smallImageKey": "",
-        "smallImageText": "",
         "startTimestamp": Date.now(),
         "endTimestamp": ""
     },
-    "twitch": {
-        "applicationID": "",
-        "url": "",
-        "details": "",
-        "state": "",
-        "largeImageKey": "",
-        "largeImageText": "",
-        "smallImageKey": "",
-        "smallImageText": "",
-        "startTimestamp": "",
-        "endTimestamp": ""
-    },
-    "spotify": {
-        "name": "",
-        "details": "",
-        "state": "",
-        "largeImageKey": "",
-        "largeImageText": "",
-        "smallImageKey": "",
-        "smallImageText": "",
-        "startTimestamp": "",
-        "endTimestamp": ""
-    }
-}
