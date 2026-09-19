6cde3d2679dd110d551ebcd0c610036393a264c5
llaef <abdoufakir970@gmail.com>
2026-09-19 10:23:49 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "token": process.env.USER_TOKEN,
+    "mode": "game",
+    "status": "online",
+
+    "game": {
+        "applicationID": process.env.APPLICATION_ID,
+        "name": "Valorant",
+        "details": "In Game - Competitive",
+        "state": "Rating: Diamond • Haven"
+    }
+};
