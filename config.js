bea500d17fa7de3725992e5c9caea5f63f8d8891
llaef <abdoufakir970@gmail.com>
2026-09-19 08:59:40 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "token": process.env.USER_TOKEN,
+    "mode": "game", 
+    "status": "online",
+
+    "game": {
+        "applicationID": "1550762686445265008",
+        "name": "Minecraft",
+        "details": "Playing Bedrock Edition",
+        "state": "Survival Mode - Multiplayer",
+        "startTimestamp": Date.now(),
+        "endTimestamp": ""
+    }
+}
