dd6673651324450ac99ad7de811a6b714e472366
llaef <abdoufakir970@gmail.com>
2026-09-19 10:14:02 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "token": process.env.USER_TOKEN,
+    "mode": "game",
+    "status": "online",
+
+    "game": {
+        "applicationID": process.env.APPLICATION_ID,
+        "name": "League of Legends",
+        "details": "In Game",
+        "state": "Ranked Solo/Duo"
+    }
+};
