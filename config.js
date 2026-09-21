b688137f6c501dd9a897311c3488dbfa54632063
llaef <abdoufakir970@gmail.com>
2026-09-21 15:58:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,18 +5,6 @@ module.exports = {
 
     "game": {
         "applicationID": process.env.APPLICATION_ID,
-        "name": "Valorant",
-        "details": "Unrated (Bind) 4-4",
-        "state": "3/5 Playing",
-        "timestamps": {
-            "start": Math.floor(Date.now() / 1000) - 894 // حساب الوقت (14:54 دقيقة)
-        },
-        "party": {
-            "id": "valorant_party_123",
-            "size": [3, 5]
-        },
-        "secrets": {
-            "join": "mystery_join_secret_code"
-        }
+        "name": "Red Dead Redemption 2",
     }
 };
